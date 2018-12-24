2ffbe8d319b1a606c8de352ce8daa9f227bc70d3
Jeffrey Pratt <clementi@users.noreply.github.com>
2018-12-24 10:52:47 -0700

    Explain rev'

--- 05/main.hs
+++ 05/main.hs
@@ -2,6 +2,7 @@ rev :: [a] -> [a]
 rev [] = []
 rev (x:xs) = (rev xs) ++ [x]
 
+-- more efficient alternative, using an auxiliary function
 rev' :: [a] -> [a]
 rev' xs = rev'' [] xs
   where rev'' acc [] = acc
